978d5772e6227566d7d47a2673677c25f495ad2e
Sandeep <sandeep@opstree.com>
2022-10-17 13:28:47 +0530

    Udated code to make log messages more verbose

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 source functions.sh
 
-echo "I'll scan the license available at [$WORKSPACE] and have mounted at [$CODEBASE_DIR]"
+logInfoMessage "I'll scan the license available at [$WORKSPACE] and have mounted at [$CODEBASE_DIR]"
 sleep  $SLEEP_DURATION
 
-echo "I've recieved below arguments $@"
+logInfoMessage "I've recieved below arguments $@"
 cd  $WORKSPACE/${CODEBASE_DIR}
 
 mkdir doc
@@ -14,10 +14,12 @@ bash -lc "license_finder"
 
 if [ $? -eq 0 ]
 then
+  logInfoMessage "Congratulations all licencses complied!!!"
   generateOutput mvn_execute true "Congratulations build succeeded!!!"
   echo "build sucessfull"
 elif  [ $? != 0 ]
 then 
+  logErrorMessage "Please check some libraries have non-complian licences!!!"
   generateOutput mvn_execute false "Build failed please check!!!!!"
   echo "build unsucessfull"
   exit 1
