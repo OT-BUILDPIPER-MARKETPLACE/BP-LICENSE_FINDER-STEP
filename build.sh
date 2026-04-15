601969b62d1d3c7d83a4b8dad40338af8cb04bc3
AniruddhD <aniporwal00@gmail.com>
2026-04-15 14:22:54 +0530

    events added

--- build.sh
+++ build.sh
@@ -6,41 +6,80 @@ source /opt/buildpiper/shell-functions/str-functions.sh
 source /opt/buildpiper/shell-functions/file-functions.sh
 source /opt/buildpiper/shell-functions/aws-functions.sh
 
+# 1. Environment Setup Phase
 if test -f requirements.txt; then
   if test ${PYTHON_VERSION:-3} = "3"; then
+    logInfoMessage "Configuring Python 3 environment for requirements.txt..."
     ln -sf $(which python3) $(which python)
     ln -sf $(which pip3) $(which pip)
+    
+    add_event "PYTHON ENV SETUP" "Successful" \
+              "Configured symlinks for Python 3" \
+              "Target: requirements.txt detected"
   fi
 fi
 
-logInfoMessage "I'll scan the license available at [$WORKSPACE] and have mounted at [$CODEBASE_DIR]"
-sleep  $SLEEP_DURATION
+# 2. Initialization Phase
+logInfoMessage "Initiating License Scanner in workspace: [$WORKSPACE], codebase directory: [$CODEBASE_DIR]"
+add_event "LICENSE SCAN INITIATED" "InProgress" \
+          "Starting license scan operations" \
+          "Target Directory: $WORKSPACE/$CODEBASE_DIR"
 
-logInfoMessage "I've recieved below arguments $@"
-cd  $WORKSPACE/${CODEBASE_DIR}
+sleep $SLEEP_DURATION
 
+logInfoMessage "Execution arguments received: $@"
+cd $WORKSPACE/${CODEBASE_DIR}
+
+# 3. Configuration Phase
 if [ -f doc/dependency_decisions.yml ]; then
-  logInfoMessage "License finder dependency decision file already exists in the repo we will be using that."
+  logInfoMessage "Existing dependency decision file detected in the repository. Using local configuration."
+  
+  add_event "DEPENDENCY CONFIGURATION" "Successful" \
+            "Loaded existing repository configuration" \
+            "File: doc/dependency_decisions.yml"
+            
   cat doc/dependency_decisions.yml
 else
-  logInfoMessage  "License finder dependency decision file doesn't exists in the repo we will be leveraging default dependency decision file."
-  mkdir doc
+  logInfoMessage "No repository dependency decision file found. Applying default configuration from /tmp."
+  mkdir -p doc
   cp /tmp/dependency_decisions.yml doc/dependency_decisions.yml
+  
+  add_event "DEPENDENCY CONFIGURATION" "Successful" \
+            "Applied default dependency configuration" \
+            "Source: /tmp/dependency_decisions.yml"
 fi
 
+# 4. Execution Phase
+logInfoMessage "Executing license_finder..."
 bash -lc "license_finder"
 
+# 5. Validation and Result Phase
 if [ $? -eq 0 ]
 then
-  logInfoMessage "Congratulations all licenses complied!!!"
+  logInfoMessage "License compliance check passed successfully. All licenses comply."
+  
+  add_event "LICENSE SCAN COMPLETE" "Successful" \
+            "All dependencies comply with approved licenses" \
+            "Action: PASSED"
+            
   generateOutput mvn_execute true "Congratulations all licenses complied!!!"
-elif [ $VALIDATION_FAILURE_ACTION == "FAILURE" ]
-  then
-    logErrorMessage "Please check some libraries have non-compliant licences!!!"
-    generateOutput mvn_execute false "Please check some libraries have non-compliant licences!!!!!"
-    logErrorMessage "build unsucessfull"
-    exit 1
-   else
-    logWarningMessage "Please check some libraries have non-compliant licences!!!"
-    generateOutput mvn_execute true "Please check some libraries have non-compliant licences!!!!!"
+elif [ "$VALIDATION_FAILURE_ACTION" == "FAILURE" ]
+then
+  logErrorMessage "License compliance check failed: Non-compliant libraries detected."
+  
+  add_event "LICENSE SCAN COMPLETE" "Failed" \
+            "Non-compliant licenses found in dependencies" \
+            "Action: BUILD FAILED (Strict Mode)"
+            
+  generateOutput mvn_execute false "Please check some libraries have non-compliant licences!!!!!"
+  logErrorMessage "Build unsuccessful due to license violations."
+  exit 1
+else
+  logWarningMessage "License compliance check finished with warnings: Non-compliant libraries detected, but build is configured to proceed."
+  
+  add_event "LICENSE SCAN COMPLETE" "Warning" \
+            "Non-compliant licenses found in dependencies" \
+            "Action: PROCEEDING (Warning Mode)"
+            
+  generateOutput mvn_execute true "Please check some libraries have non-compliant licences!!!!!"
 fi
